ad17f776b298d2751d78eb358f202acd6658c62a
wolanski_mateusz <94111142+wolanskimateusz@users.noreply.github.com>
2024-11-09 16:01:31 +0100

    upgraded hero

--- front/frontend_inz/src/App.tsx
+++ front/frontend_inz/src/App.tsx
@@ -1,3 +1,4 @@
+import Card from "./Components/Card/Card"
 import Header from "./Components/Header/Header"
 import Hero from "./Components/Hero/Hero"
 
@@ -7,7 +8,12 @@ function App() {
   return (
     <>
     <Header></Header>
-    <Hero></Hero>
+    <Hero>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+    </Hero>
+    
     </>
   )
 }
--- front/frontend_inz/src/Components/Hero/Hero.tsx
+++ front/frontend_inz/src/Components/Hero/Hero.tsx
@@ -1,16 +1,42 @@
+
+import { ReactNode } from 'react'
 import './Hero.css'
-function Hero()
+import HeroImage from './HeroImage.jpg'
+
+interface HeroProps {
+    children?: ReactNode; 
+  }
+
+function Hero({children}: HeroProps)
 {
-    return <>
-        <div className='hero-image'>
-            <div className="d-flex justify-content-center">
-            <h1 className='text-light text-center text-wrap' style={{width:'35rem'}}>
-                Przeżyj niezapomniane emocje z twoimi ulubionymi artystami
-            </h1>
-            <a href="#" className="btn btn-danger">Przeglądaj wydarzenia</a>
-            </div>
+    return <div className='d-flex justify-content-center bg-dark py-3'>
+        <div
+      className="bg-image d-flex flex-column align-items-center justify-content-start pt-5"
+      style={{
+        backgroundImage: `url(${HeroImage})`,
+        height: '100vh',
+        width:'90%',
+        borderRadius: '25px',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+      }} >
+        <div className='text-white fw-bold  h-25 display-4 p-5'>
+            Przeżyj niezapomniane emocje z twoimi ulubionymi artystami
+         </div>
+         <a href="#" className="btn btn-danger btn-lg m-5"
+         style={{
+            fontSize: '1.5rem', 
+            padding: '20px 40px',
+          }}>
+            Przeglądaj wydarzenia
+        </a>
+         <p className="text-white fs-2 text-decoration-underline mt-5">Polecane wydarzenia</p>
+         <div className=' d-flex flex-row  justify-content-center'>
+            {children}
         </div>
-    </>
+        </div>
+        
+    </div>
 }
 
 export default Hero
